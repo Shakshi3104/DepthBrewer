e018b97e31a5587b3b57ddc693a912a9c36da26d
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-11-07 11:08:24 +0900

    Initial Commit

---
+++ Shared/DepthBrewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepthBrewerApp.swift
+//  Shared
+//
+//  Created by MacBook Pro on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct DepthBrewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
